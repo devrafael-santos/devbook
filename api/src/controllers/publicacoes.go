a0172c575ce1f97b51a67af8b85cc5db85009a9a
devrafael-santos <raafael.cs@gmail.com>
2024-07-16 21:55:14 -0300

    feat

---
+++ api/src/controllers/publicacoes.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "net/http"
+
+func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
